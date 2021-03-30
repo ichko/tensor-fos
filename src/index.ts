04c720733de604a6580d23905e78ee7a0d94ec9e
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-03-30 22:44:47 +0300

    Load mnist digits

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as d3 from 'd3';
 import * as tf from '@tensorflow/tfjs';
+import { MNISTDataset } from 'tfjs-data-mnist';
 
 import { NDTensorHeatmapVisualizer } from './visualizers/NDTensorHeatmapVisualizer';
 
-window.onload = () => {
+window.onload = async () => {
+  const ds = await MNISTDataset.create();
+
+  const it = await ds.testDataset.iterator();
+
   d3.select('body')
     .append('button')
     .text('Reload')
     .on('click', () => main());
 
-  const shape = [3, 2, 2, 3, 10, 10];
+  const shape = [28, 28];
   const t = tf.randomUniform(shape);
   const vis = new NDTensorHeatmapVisualizer({
     pixelSize: 10,
@@ -20,9 +25,10 @@ window.onload = () => {
   const canvas = vis.getHTMLElement();
   document.body.append(canvas);
 
-  function main() {
-    const t = tf.randomNormal(shape);
-    vis.setTensor(t);
+  async function main() {
+    const dataElement = await it.next();
+    const digit = tf.tensor(dataElement.value[0], [28, 28]);
+    vis.setTensor(digit);
   }
 
   main();
