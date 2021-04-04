56ab7004a4b1155c5134b681097a29cc5fa5af6f
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-04-04 15:36:52 +0300

    Refactor interactive tensor

--- src/interactive-tensor.ts
+++ src/interactive-tensor.ts
@@ -1,35 +1,16 @@
 import { Tensor } from '@tensorflow/tfjs-core';
 import QuickSettings from 'quicksettings';
-import { NDTensorHeatmapVisualizer } from './visualizers/heatmap';
-import { debounce } from 'debounce';
+import { TensorVisualizer } from './visualizers/tensor-visualizer';
 
-type VisType = 'heatmap';
-
-export class InteractiveTensor {
-  private vis: NDTensorHeatmapVisualizer;
-
-  constructor(shape: number[], type: VisType = 'heatmap') {
+export class InteractiveTensor<Config> {
+  constructor(private vis: TensorVisualizer<Config>) {
     // QuickSettings.useExtStyleSheet();
-    const settings = QuickSettings.create(0, 0, 'Tensor');
-
-    this.vis = new NDTensorHeatmapVisualizer({
-      pixelSize: 2,
-      outerPadding: 2,
-    });
+    const settings = QuickSettings.create(50, 50, 'Tensor');
+    const anySettings = settings as any;
+    anySettings._panel.style.width = 'auto';
 
     const canvas = this.vis.domElement;
     settings.addElement('', canvas);
-
-    const onVal = (val: number) => {
-      console.log(val);
-      this.vis.set({ pixelSize: val });
-      const width = canvas.getBoundingClientRect().width + 20;
-      settings.setWidth(width);
-    };
-
-    onVal(3);
-
-    settings.addNumber('padding', 1, 5, 3, 1, onVal);
   }
 
   setTensor(tensor: Tensor) {
