7295d5b748cefdf8d549e111645f9ab7f8e499dc
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-04-11 01:53:09 +0300

    Remove unnecessary inputs

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
-import * as d3 from 'd3';
 import * as tf from '@tensorflow/tfjs';
 import { MNISTDataset } from 'tfjs-data-mnist';
 
-import { Tensor } from '@tensorflow/tfjs';
-import { D3fcSeriesVisualizer } from './visualizers/d3fc-series';
-import { HeatmapVisualizer } from './visualizers/heatmap';
-import { SmallMultiplesVisualizer } from './visualizers/small-multiples';
-import { UPlotVisualizer } from './visualizers/uplot-visualizer';
-import { makeMenu, makeStats } from './ui';
+import { makeUI, makeStats } from './ui';
 
 window.onload = async () => {
-  makeMenu();
-  tf.backend();
+  makeUI();
+  tf.backend(); // Register backend
 
   const stats = makeStats();
-
-  const uplotVis = new UPlotVisualizer();
-
-  d3.select('body')
-    .append('button')
-    .text('Reload')
-    .on('click', () => main());
 
   let t = 0;
   async function main() {
