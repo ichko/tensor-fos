d7ac56af52a86ff20734997a942d3acb092b77bb
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-04-20 00:54:31 +0300

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
 // require('dotenv').config();
 
-// import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
 import { makeUI, makeStats } from './ui';
 import { Editor } from './editor';
-import { FlumeEditor } from './editor/flume';
 
 window.onload = async () => {
   const editor = new Editor();
   document.body.appendChild(editor.domElement);
 
-  // tf.backend(); // Register backend
-  // makeUI();
-  // const p = parallel(() => {});
-  // p();
-  // const stats = makeStats();
-  // let t = 0;
-  // async function main() {
-  //   t += 1;
-  // }
-  // async function animate() {
-  //   stats.begin();
-  //   // main();
-  //   stats.end();
-  //   requestAnimationFrame(animate);
-  // }
-  // requestAnimationFrame(animate);
-  // main();
+  tf.backend(); // Register backenad
+  makeUI();
+
+  const stats = makeStats();
+
+  let t = 0;
+  async function main() {
+    t += 1;
+  }
+  async function animate() {
+    stats.begin();
+    main();
+    stats.end();
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+  main();
 };
