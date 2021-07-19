21a4dd246226bf5488d21e98876fec9c1d5d220b
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-07-19 23:10:50 +0300

    Add simple function in generated tf module

--- src/editor/nodes-to-generate/tf.ts
+++ src/editor/nodes-to-generate/tf.ts
@@ -2,3 +2,13 @@ export { layers as tfLayers } from '@tensorflow/tfjs';
 
 export type { ActivationIdentifier } from '@tensorflow/tfjs-layers/dist/keras_format/activation_config';
 export type { DenseLayerArgs } from '@tensorflow/tfjs-layers/dist/layers/core';
+
+import { reshape } from '@tensorflow/tfjs';
+
+export const tfCommon = {
+  reshape,
+};
+
+export function add({ a, b }: { a: number; b: number }) {
+  return { c: a + b };
+}
