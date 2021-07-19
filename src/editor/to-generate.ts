56913c2a5860e733dbe3bd6db2c8758f19aba77a
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-07-20 00:21:34 +0300

    Reorder type file from which generated json is computed

--- src/editor/to-generate.ts
+++ src/editor/to-generate.ts
@@ -1,14 +1,20 @@
+import { Tensor } from '@tensorflow/tfjs-core';
 export { layers as tfLayers } from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
 
 export type { ActivationIdentifier } from '@tensorflow/tfjs-layers/dist/keras_format/activation_config';
 export type { DenseLayerArgs } from '@tensorflow/tfjs-layers/dist/layers/core';
 
-import { reshape } from '@tensorflow/tfjs';
-
-export const tfCommon = {
-  reshape,
-};
-
 export function add({ a, b }: { a: number; b: number }) {
   return { c: a + b };
+}
+
+export function reshape({
+  tensor,
+  shape,
+}: {
+  tensor: Tensor;
+  shape: number[];
+}) {
+  return { out: tf.reshape(tensor, shape) };
 }
