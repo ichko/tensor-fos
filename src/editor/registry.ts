93d3f550a5576bcaf6cb488dd8cfdaa17fecad2a
Iliya Zhecev <iliya.zhechev@gmail.com>
2021-07-21 23:38:10 +0300

    Remove unnecessary stuff from registry

--- src/editor/registry.ts
+++ src/editor/registry.ts
@@ -1,106 +1,20 @@
 import { zip } from './../utils';
-import { Tensor } from '@tensorflow/tfjs-core';
-import { NodeEditor, nodeType, Port } from '.';
-import * as ml from '../ml';
-import { SmallMultiplesRenderer } from '../tensor-renderer/small-multiples';
-import { TfJsVisRenderer } from '../tensor-renderer/tfjs-vis';
-import * as tf from '@tensorflow/tfjs';
-import { memo } from '../utils';
+import { NodeEditor, nodeType } from '.';
 import { colors } from 'src/editor/to-generate';
 
-interface NodeDef {
-  func: any;
-  args?: Port<any>[];
-}
-
-function exportTfJsNodes() {
-  const objectInputArgDefs: NodeDef[] = [
-    {
-      func: tf.layers.dense,
-      args: [{ name: 'units', type: 'int', defaultValue: 10 }],
-    },
-    {
-      func: tf.layers.flatten,
-      args: [],
-    },
-    {
-      func: tf.layers.embedding,
-      args: [
-        { name: 'inputDim', type: 'int', defaultValue: 10 },
-        { name: 'outputDim', type: 'int', defaultValue: 10 },
-      ],
-    },
-  ];
-  const sequentialInputArgsDefs: NodeDef[] = [
-    {
-      func: tf.rand,
-      args: [{ name: 'shape', type: 'json', defaultValue: [3, 5, 5] }],
-    },
-    {
-      func: tf.randomNormal,
-      args: [{ name: 'shape', type: 'json', defaultValue: [3, 5, 5] }],
-    },
-    {
-      func: tf.randomUniform,
-      args: [{ name: 'shape', type: 'json', defaultValue: [3, 5, 5] }],
-    },
-  ];
-
-  const objectInputArgNodes = objectInputArgDefs.map(def =>
-    nodeType({
-      color: colors.model,
-      id: `tf.layers.${def.func.name}`,
-      ins: def.args?.map(arg => ({ name: arg.name as string, type: arg.type })),
-      outs: ['layer'],
-      ctor: async () => {
-        return {
-          compute: memo(async (args: any) => {
-            const layer = def.func(args);
-            return { layer };
-          }),
-        };
-      },
-    })
-  );
-
-  const sequentialInputArgsNodes = sequentialInputArgsDefs.map(def =>
-    nodeType({
-      color: colors.util,
-      id: `tf.${def.func.name}`,
-      ins: def.args?.map(arg => ({ name: arg.name as string, type: arg.type })),
-      outs: ['tensor'],
-      ctor: async () => {
-        return {
-          compute: async (args: any) => {
-            const tensor = def.func(...Object.values(args));
-            return { tensor };
-          },
-        };
-      },
-    })
-  );
-
-  return [...objectInputArgNodes, ...sequentialInputArgsNodes];
-}
-
 export async function registerNodeTypes(editor: NodeEditor) {
-  const tfjsNodeTypes = exportTfJsNodes();
   const generated = await getGeneratedNodeTypes();
 
-  [...tfjsNodeTypes, ...generated].forEach(nodeType => {
+  [...generated].forEach(nodeType => {
     editor.registerNodeType(nodeType as any);
   });
 }
 
 async function getGeneratedNodeTypes() {
-  const generatedDocs = require('../../dist/generated-type-docs.json');
-  console.log('input >>', generatedDocs);
-
+  const generatedDocs = require('/dist/generated-type-docs.json');
   const module = await require('src/editor/to-generate');
 
   function resolveType(type: any): any {
-    console.log('T', type);
-
     switch (type.kindString) {
       case 'Project':
         return type.children.flatMap(resolveType).filter((c: any) => !!c);
@@ -170,8 +84,5 @@ async function getGeneratedNodeTypes() {
     }
   }
 
-  const resolutions = resolveType(generatedDocs);
-
-  console.log('resolution >>', resolutions);
-  return resolutions;
+  return resolveType(generatedDocs);
 }
